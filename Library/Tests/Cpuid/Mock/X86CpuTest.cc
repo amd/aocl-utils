1b5532f3a3a00c238a4ddb2a1f3734aae7085979
Christina Jacob <Christina.Jacob@amd.com>
2024-04-30 12:05:02 +0530

    Utils:Test Document the hardcoded value

--- Library/Tests/Cpuid/Mock/X86CpuTest.cc
+++ Library/Tests/Cpuid/Mock/X86CpuTest.cc
@@ -41,7 +41,9 @@ class MockX86Cpu
   protected:
     void SetUp() override
     {
-        EXPECT_CALL(mockCpuidUtils, __raw_cpuid(testing::_)).Times(12);
+        // The number of times __raw_cpuid expected to in the code flow.
+        auto callCount = 12;
+        EXPECT_CALL(mockCpuidUtils, __raw_cpuid(testing::_)).Times(callCount);
     }
 };
 
