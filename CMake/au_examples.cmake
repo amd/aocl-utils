0a6724be170f740fc28fa4e088c2a6d5dc2d3284
Christina Jacob <Christina.Jacob@amd.com>
2024-05-28 11:05:48 +0530

    Cpuid example : Proper use of au_examples
    Fix au_examples to have INCLUDES parameter as it advertises,
    Use it in Examples CMakeLists.txt

--- CMake/au_examples.cmake
+++ CMake/au_examples.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023, Advanced Micro Devices. All rights reserved.
+# Copyright (C) 2022-2024, Advanced Micro Devices. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -59,6 +59,9 @@ function(au_add_application NAME)
         ${APP_LIBS}
         au::core
     )
+  endif()
+  if(DEFINED APP_INCLUDES)
+      target_include_directories(${NAME} PRIVATE ${APP_INCLUDES})
   endif()
 
 endfunction(au_add_application)
